7f5a78daf327973bd937d8b32ab5bbdc560c8259
Kaelen Juntilla <Da4kast@charter.net>
2014-12-07 00:25:02 -0800

    13th push

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -14,10 +14,14 @@ game.PlayerEntity = me.Entity.extend({
        }]);
    
        this.renderable.addAnimation("idle", [3]);
+       this.renderable.addAnimation("bigIdle", []);
        this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
+       this.renderable.addAnimation("bigWalk", [14, 15, 16, 17, 18, 19], 80);
+       
        
        this.renderable.setCurrentAnimation("idle");
        
+       this.big = false;
        this.body.setVelocity(5, 20);
        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
 
@@ -44,22 +48,46 @@ game.PlayerEntity = me.Entity.extend({
         this.body.update(delta);
         me.collision.check(this, true, this.collideHandler.bind(this), true);
        
-       if(this.body.vel.x !== 0){
-            if(!this.renderable.isCurrentAnimation("smallWalk")) {
-                this.renderable.setCurrentAnimation("smallWalk");
-                this.renderable.setAnimationFrame();
-             }
-       }else{
-           this.renderable.setCurrentAnimation("idle");
-       }
+       if(!this.big){
+           if(this.body.vel.x !== 0){
+                if(!this.renderable.isCurrentAnimation("smallWalk")) {
+                    this.renderable.setCurrentAnimation("smallWalk");
+                    this.renderable.setAnimationFrame();
+                 }
+           }else{
+               this.renderable.setCurrentAnimation("idle");
+           }
+        }else{
+            if(!this.big){
+           if(this.body.vel.x !== 0){
+                if(!this.renderable.isCurrentAnimation("bigWalk")) {
+                    this.renderable.setCurrentAnimation("bigWalk");
+                    this.renderable.setAnimationFrame();
+                 }
+           }else{
+               this.renderable.setCurrentAnimation("bigIdle");
+           }
+        }
        
        
        
        this._super(me.Entity, "update", [delta]);
        return true;
-   },
+   }
    
    collideHandler: function(response){
+       var ydif = this.pos.y - response.b.pos.y;
+       
+       //response.b represents what mario is running into in this case it is badguy//
+       if(response.b.type === 'badguy'){
+           if(ydif <= -115){
+               response.b.alive = false;
+           }else
+               
+               me.state.change(me.state.MENU);
+       }else if(response.b.type === 'mushroom'){
+           this.big = true;
+       }
        
    }
    
@@ -103,26 +131,30 @@ game.BadGuy = me.Entity.extend({
        this.spritewidth = 60;
        var width = settings.width;
        x = this.pos.x;
+       //this is the starting point for the bad guy//
        this.startX = x;
        this.endX = x + width - this.spritewidth;
        this.pos.x = x + width -this.spritewidth;
        
        this.updateBounds();
-       
+       //this code says that even if we are on the screen the badguy should be updated this also adds performance issues to the game//
        this.alwaysUpdate = true;
-       
+       //this tells the game whether our character is walking left or right//
        this.walkLeft = false;
+       //this tells the game if our badguy is alive//
        this.alive = true;
+       //this will determine whether what mario walks into different things will happen than walking into something like a mushroom//
        this.type = "badguy";
        
        this.renderable.addAnimation("run", [0, 1, 2], 80);
        this.renderable.setCurrentAnimation("run");
-    
+       //this tells how fast our badguy will be going//
        this.body.setVelocity(4, 6);
     },
     
     update: function(delta){
         this.body.update(delta);
+        //we are checking if we are colliding with the slime it would go to the collideHandler to see if we have collided with the slime//
         me.collision.check(this, true, this.collideHandler.bind(this), true);
         
         if(this.alive){
@@ -130,8 +162,9 @@ game.BadGuy = me.Entity.extend({
                 this.walkLeft = false;
             }else if(!this.walkLeft && this.pos.x >= this.endX){
                 this.walkLeft = true;
-            }
+            }//this tells the animation/badguy which direction he is facing//
             this.flipX(!this.walkLeft);
+            //this line of code tells if the statement is true, do whatever is on the left side of the colon, and if this statement is false do whatever is on the right side of the colon.
             this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.timer.tick : this.body.accel.x * me.timer.tick;
             
         }else{
@@ -147,6 +180,26 @@ game.BadGuy = me.Entity.extend({
         
     }
 
+
+});
+
+game.Mushroom = me.Entity.extend({
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+                image: "mushroom",
+                spritewidth: "64",
+                spriteheight: "64",
+                width: 64,
+                height: 64,
+                getShape: function() {
+                    return (new me.Rect(0, 0, 64, 64)).toPolygon();
+                }
+
+            }]);
+        
+        me.collision.check(this);
+        this.type = "mushroom";
+    }
 
 });
     
